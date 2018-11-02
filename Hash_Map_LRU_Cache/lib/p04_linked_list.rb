a6fcbeb1c1b502bb230b81b175cc0b5c47b6af6d
misolee <mzxmiso@gmail.com>
2018-11-02 16:11:23 -0700

    Create LinkedList

---
+++ Hash_Map_LRU_Cache/lib/p04_linked_list.rb
@@ -0,0 +1,112 @@
+class Node
+  attr_reader :key
+  attr_accessor :val, :next, :prev
+
+  def initialize(key = nil, val = nil)
+    @key = key
+    @val = val
+    @next = nil
+    @prev = nil
+  end
+
+  def to_s
+    "#{@key}: #{@val}"
+  end
+
+  def remove
+    # optional but useful, connects previous link to next link
+    # and removes self from list.
+  end
+end
+
+class LinkedList
+  include Enumerable
+
+  def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
+  end
+
+  def [](i)
+    each_with_index { |link, j| return link if i == j }
+    nil
+  end
+
+  def first
+    @head.next
+  end
+
+  def last
+    @tail.prev
+  end
+
+  def empty?
+    @head.next == @tail
+  end
+
+  def get(key)
+    self.each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+    nil
+  end
+
+  def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
+  end
+
+  def append(key, val)
+    new_node = Node.new(key, val)
+
+    prev_node = @tail.prev
+    prev_node.next = new_node
+    new_node.prev = prev_node
+
+    new_node.next = @tail
+    @tail.prev = new_node
+
+    new_node
+  end
+
+  def update(key, val)
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        break
+      end
+    end
+  end
+
+  def remove(key)
+    self.each do |node|
+      if node.key == key
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+      end
+    end
+  end
+
+  def each
+    current_node = @head
+    until current_node == @tail.prev
+      next_node = current_node.next
+      yield next_node
+      current_node = next_node
+    end
+  end
+
+  # uncomment when you have `each` working and `Enumerable` included
+  # def to_s
+  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  # end
+end
